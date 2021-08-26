3ba4275b972e577dd7b8a837bee14c80a0522775
ASUS <wongjj-am18@student.tarc.edu.my>
2021-08-26 17:23:26 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/warehouserit3g2/MainActivity.kt
@@ -0,0 +1,11 @@
+package my.edu.tarc.warehouserit3g2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
